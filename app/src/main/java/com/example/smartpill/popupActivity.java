d0ac6e1bf8b07805353834610f90380d70e67a3d
cmdatan <christian.datan@eee.upd.edu.ph>
2019-04-23 22:56:59 +0800

    commit_sprint_2

--- app/src/main/java/com/example/smartpill/popupActivity.java
+++ app/src/main/java/com/example/smartpill/popupActivity.java
@@ -3,20 +3,33 @@ package com.example.smartpill;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class popupActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class popupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_popup);
-        /*
+
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*0.8),(int)(height*.7));
-        */
+        getWindow().setLayout((int)(width*0.9),(int)(height*.6));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = 0;
+
+        getWindow().setAttributes(params);
+
     }
 }
